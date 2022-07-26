b70ca21524923d47e9307fc3111d9f56ec0a0b5e
abe131313 <abrarmohammed27@gmail.com>
2022-07-26 22:51:28 +0530

    Modal and rating
    Created the modal and rating using which a user could rate a artist and add an artist

--- src/App.js
+++ src/App.js
@@ -3,19 +3,50 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState,useEffect } from 'react';
 import {db} from './firebase-config';
 import {collection,getDocs,addDoc,updateDoc,doc,deleteDoc} from 'firebase/firestore';
-import CreatArtist from './components/CreateArtistInfo';
+
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import BasicTable from './components/table'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 1000,
+  height:500,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 // artwork,song,dateofrelease,artists,rating
 
 
-function App() {
+function App(props) {
+  
   const artistCollectionRef = collection(db,'artistinfo');
   const [artistInfo,setArtistInfo] = useState([])
   const [song,setSong] = useState("");
   const[dateofrelease,setDateOfRelease] = useState("")  
   const[artist,setArtist] = useState("")
+  const [value, setValue] = React.useState(2);
+  
+
+
+  
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
   const createArtist = async() => {
-    await addDoc(artistCollectionRef,{artist:artist,dateofrelease:dateofrelease,song:song});
+    await addDoc(artistCollectionRef,{artist:artist,dateofrelease:dateofrelease,song:song,rating:value});
   }
 
   useEffect(() => {
@@ -42,11 +73,51 @@ function App() {
             </form>
           </div>
       </nav>
-      <h3>
-        Create Artists..
-      </h3>
-      <CreatArtist/>
+      <div>
+      <Button onClick={handleOpen}>+ Add Artist</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+            <div class="m-5">
+                <h3>Fill the details</h3>
+                <input placeholder='Name of the artist' onChange={(event) => {setArtist(event.target.value)}}></input>
+                <input onChange={(event) => {setSong(event.target.value)}} placeholder='song'></input>
+                <input onChange={(event) => {setDateOfRelease(event.target.value)}} placeholder='date of release'></input>
+                
+                <div>
+                  <Box
+                      sx={{
+                          '& > legend': { mt: 2 },
+                      }}
+                      >
+                      <Typography component="legend"></Typography>
+                      <Rating
+                          name="simple-controlled"
+                          value={value}
+                          onChange={(event, newValue) => {
+                          setValue(newValue);
+                          }}
+                      />
+                  </Box>
+                </div>
+
+                <button onClick={createArtist} type="button" class="btn btn-secondary mx-1">submit</button>
+            </div>
+        </Box>
+
+      </Modal>
     </div>
+
+
+      {/* <BasicModal artistName = {(event) => {setArtist(event.target.value)}} song = {(event) => {setSong(event.target.value)}} releaseDate = {(event) => {setDateOfRelease(event.target.value)}} btnHandler = {createArtist} /> */}
+
+      <BasicTable/>
+    </div>
+      
     
   );
 }
